c3b8aa9cfd59886a7ebcabe619697e78049599cb
FrancisPog1 <allanbelenisko@gmail.com>
2024-03-24 20:14:55 +0800

    New Commits

--- Accreditation/views_instrument_level_folder.py
+++ Accreditation/views_instrument_level_folder.py
@@ -11,7 +11,7 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
  
 @login_required
-def landing_page(request, pk):
+def parent_landing_page(request, pk):
     #Getting the data from the API
     create_form = Create_InstrumentDirectory_Form(request.POST or None)
     records = instrument_level_folder.objects.filter(is_deleted= False, instrument_level=pk, parent_directory= None) #Getting all the data inside the Program table and storing it to the context variable
@@ -35,7 +35,34 @@ def landing_page(request, pk):
                 'pk': pk
                }  
 
-    return render(request, 'accreditation-level-directory/main-page/landing-page.html', context)
+    return render(request, 'accreditation-level-parent-directory/main-page/landing-page.html', context)
+
+@login_required
+def child_landing_page(request, pk):
+    #Getting the data from the API
+    create_form = Create_InstrumentDirectory_Form(request.POST or None)
+    records = instrument_level_folder.objects.filter(is_deleted= False, parent_directory=pk) #Getting all the data inside the Program table and storing it to the context variable
+    parent_folder = instrument_level_folder.objects.get(is_deleted=False, id=pk) #Getting the data of the parent folder
+    # Initialize an empty list to store update forms for each record
+    details = []
+
+    # Iterate through each record and create an update form for it
+    for record in records:
+        update_form = Create_InstrumentDirectory_Form(instance=record)
+        created_by = record.created_by  # Get the user who created the record
+        modified_by = record.modified_by  # Get the user who modified the record
+        details.append((record, update_form, created_by, modified_by))
+
+    #Getting all the data inside the type table and storing it to the context variable
+    context = { 'records': records, 
+                'create_form': create_form, 
+                'details': details,
+                'pk': pk,
+                'parent_folder': parent_folder
+               }  
+
+    return render(request, 'accreditation-level-child-directory/main-page/landing-page.html', context)
+
 
 @login_required
 def create(request, pk):
@@ -67,6 +94,134 @@ def create(request, pk):
     else:
         # Return a validation error using a JSON response
         return JsonResponse({'errors': create_form.errors}, status=400)
+    
+@login_required
+def create_child(request, pk):
+    create_form = Create_InstrumentDirectory_Form(request.POST or None)
+
+    if create_form.is_valid():
+        create_form.instance.created_by = request.user
+        create_form.instance.parent_directory_id = pk
+        create_form.save()
+        name = create_form.cleaned_data.get('name')
+
+        # Create an instance of the ActivityLog model
+        activity_log_entry = activity_log()
+
+        # Set the attributes of the instance
+        activity_log_entry.module = "INSTRUMENT LEVEL FOLDERS MODULE"
+        activity_log_entry.action = "Created a record"
+        activity_log_entry.type = "CREATE"
+        activity_log_entry.datetime_acted =  timezone.now()
+        activity_log_entry.acted_by = request.user
+        # Set other attributes as needed
+
+        # Save the instance to the database
+        activity_log_entry.save()
+
+        
+        messages.success(request, f'{name} is successfully created!') 
+        return JsonResponse({'status': 'success'}, status=200)
+    else:
+        # Return a validation error using a JSON response
+        return JsonResponse({'errors': create_form.errors}, status=400)
+
+
+@login_required
+def update(request, pk):
+    # Retrieve the type object with the given primary key (pk)
+    try:
+        folder_record = instrument_level_folder.objects.get(id=pk)
+    except instrument_level_folder.DoesNotExist:
+        return JsonResponse({'errors': 'Folder is not found!'}, status=404)
+
+    if request.method == 'POST':
+        # Process the form submission with updated data
+        update_form = Create_InstrumentDirectory_Form(request.POST or None, instance=folder_record)
+
+        if update_form.is_valid():
+            # Save the updated data to the database
+            update_form.instance.modified_by = request.user
+            update_form.save()  
+            name = update_form.cleaned_data.get('name')
+
+            # Create an instance of the ActivityLog model
+            activity_log_entry = activity_log()
+
+            # Set the attributes of the instance
+            activity_log_entry.module = "INSTRUMENT LEVEL FOLDERS MODULE"
+            activity_log_entry.action = "Modified a record"
+            activity_log_entry.type = "UPDATE"
+            activity_log_entry.datetime_acted =  timezone.now()
+            activity_log_entry.acted_by = request.user
+            # Set other attributes as needed
+
+            # Save the instance to the database
+            activity_log_entry.save()
+
+            # Provide a success message as a JSON response
+            messages.success(request, f'{name} is successfully updated!') 
+            return JsonResponse({'status': 'success'}, status=200)
+
+        else:
+            # Return a validation error using a JSON response
+            return JsonResponse({'errors': update_form.errors}, status=400)
         
 
+@login_required
+def archive(request, pk, level_id):
+    # Gets the records who have this ID
+    folder_record = instrument_level_folder.objects.get(id=pk)
 
+    #After getting that record, this code will delete it.
+    folder_record.modified_by = request.user
+    folder_record.is_deleted=True
+    folder_record.deleted_at = timezone.now()
+    name = folder_record.name
+    folder_record.save()
+
+    # Create an instance of the ActivityLog model
+    activity_log_entry = activity_log()
+
+    # Set the attributes of the instance
+    activity_log_entry.module = "INSTRUMENT LEVEL FOLDERS MODULE"
+    activity_log_entry.action = "Archived a record"
+    activity_log_entry.type = "ARCHIVE"
+    activity_log_entry.datetime_acted =  timezone.now()
+    activity_log_entry.acted_by = request.user
+    # Set other attributes as needed
+
+    # Save the instance to the database
+    activity_log_entry.save()
+
+    messages.success(request, f'The folder named "{name}" is successfully archived!') 
+    return redirect('accreditations:instrument-level-directory', pk=level_id)
+
+@login_required
+def archive_child(request, pk, parent_id):
+    # Gets the records who have this ID
+    folder_record = instrument_level_folder.objects.get(id=pk)
+
+    #After getting that record, this code will delete it.
+    folder_record.modified_by = request.user
+    folder_record.is_deleted=True
+    folder_record.deleted_at = timezone.now()
+    name = folder_record.name
+    folder_record.save()
+
+    # Create an instance of the ActivityLog model
+    activity_log_entry = activity_log()
+
+    # Set the attributes of the instance
+    activity_log_entry.module = "INSTRUMENT LEVEL FOLDERS MODULE"
+    activity_log_entry.action = "Archived a record"
+    activity_log_entry.type = "ARCHIVE"
+    activity_log_entry.datetime_acted =  timezone.now()
+    activity_log_entry.acted_by = request.user
+    # Set other attributes as needed
+
+    # Save the instance to the database
+    activity_log_entry.save()
+
+    messages.success(request, f'The folder named "{name}" is successfully archived!') 
+    return redirect('accreditations:instrument-level-child-directory', pk=parent_id)
