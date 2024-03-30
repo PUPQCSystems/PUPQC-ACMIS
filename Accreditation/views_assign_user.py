5b0ff1c4a4adcb348a199fc290b6ea48d21b98ea
FrancisPog1 <allanbelenisko@gmail.com>
2024-03-31 04:19:45 +0800

    asdsadas

--- Accreditation/views_assign_user.py
+++ Accreditation/views_assign_user.py
@@ -1,21 +1,32 @@
+from django.db import IntegrityError
+from django.http import JsonResponse
+from Users.models import CustomUser, activity_log
+from .models import instrument_level_area, instrument_level_folder, user_assigned_to_folder #Import the model for data retieving
+from .forms import ChairManAssignedToFolder_Form, CoChairUserAssignedToFolder_Form, MemberAssignedToFolder_Form
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required, permission_required
+from django.db import transaction
+
 @login_required
-@permission_required(["Accreditation.add_user_assigned_to_area", "Accreditation.change_user_assigned_to_area", "Accreditation.view_user_assigned_to_area", "Accreditation.delete_user_assigned_to_area"], raise_exception=True)
+@permission_required(["Accreditation.add_user_assigned_to_folder", "Accreditation.change_user_assigned_to_folder", "Accreditation.view_user_assigned_to_folder", "Accreditation.delete_user_assigned_to_folder"], raise_exception=True)
 def assign_user(request):
     if request.method == "POST":
-        chairman_form = ChairManAssignedToArea_Form(request.POST)
-        cochairman_form = CoChairUserAssignedToArea_Form(request.POST)
+        chairman_form = ChairManAssignedToFolder_Form(request.POST)
+        cochairman_form = CoChairUserAssignedToFolder_Form(request.POST)
         try:
             with transaction.atomic():
                 chairman_val = request.POST.get('is_chairman')
                 print(chairman_val)
                 cochairman_val = request.POST.get('is_cochairman')
                 member_list = request.POST.getlist('is_member')
-                area = instrument_level_area.objects.get(id=int(request.POST.get('area')))
+                folder_pk = request.POST.get('folder')
+                folder = instrument_level_folder.objects.get(id=folder_pk)
         
-                existing_chairman = user_assigned_to_area.objects.filter(area=area, is_chairman=True).first()
-                existing_cochairman = user_assigned_to_area.objects.filter(area=area, is_cochairman=True).first()
-                existing_members = user_assigned_to_area.objects.filter(area=area, is_member=True)
-                existing_assignment = user_assigned_to_area.objects.filter(area=area)
+                existing_chairman = user_assigned_to_folder.objects.filter(parent_directory=folder, is_chairman=True).first()
+                existing_cochairman = user_assigned_to_folder.objects.filter(parent_directory=folder, is_cochairman=True).first()
+                existing_members = user_assigned_to_folder.objects.filter(parent_directory=folder, is_member=True)
+                existing_assignment = user_assigned_to_folder.objects.filter(parent_directory=folder)
 
                 if existing_assignment:
                     if chairman_val and cochairman_val and cochairman_val != 'None' and existing_chairman and existing_cochairman and existing_members:
@@ -73,7 +84,7 @@ def assign_user(request):
                     return JsonResponse({'error': 'Please select a Chairman for this Area.'}, status=400)
                 else:
                     chairman = CustomUser.objects.get(id=int(chairman_val))
-                    existing_chairmen = user_assigned_to_area.objects.filter(area=area, is_chairman=True)
+                    existing_chairmen = user_assigned_to_folder.objects.filter(parent_directory=folder, is_chairman=True)
 
                     # Check if there are existing chairmen for the area
                     if existing_chairmen.exists():
@@ -90,7 +101,7 @@ def assign_user(request):
                 
                 if cochairman_val is not None and cochairman_val != 'None':
                     cochairman = CustomUser.objects.get(id=cochairman_val)
-                    existing_cochairmen = user_assigned_to_area.objects.filter(area=area, is_cochairman=True)
+                    existing_cochairmen = user_assigned_to_folder.objects.filter(parent_directory=folder, is_cochairman=True)
                     
                     # Check if there are existing co-chairmen for the area
                     if existing_cochairmen.exists():
@@ -108,13 +119,13 @@ def assign_user(request):
                 if member_list:
                     if len(member_list) <= 5:
                         member_ids = [int(member_id) for member_id in member_list]  # Convert string IDs to integers
-                        conflicting_members = user_assigned_to_area.objects.filter(area=area, is_member=True)
+                        conflicting_members = user_assigned_to_folder.objects.filter(parent_directory=folder, is_member=True)
                         if conflicting_members.exists():
                             conflicting_members.delete()
                         
                         for member_id in member_ids:
                             member = CustomUser.objects.get(id=member_id)
-                            member_form_instance = MemberAssignedToArea_Form(request.POST)
+                            member_form_instance = MemberAssignedToFolder_Form(request.POST)
                             member_form_instance.instance.assigned_user = member
                             member_form_instance.instance.area = area
                             member_form_instance.instance.is_member = True
